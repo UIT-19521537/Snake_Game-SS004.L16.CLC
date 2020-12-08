d8df55f63a10a9300851cac311cc7d25cadf733d
UIT19521537 <19521537@gm.uit.edu.vn>
2020-12-09 00:46:05 +0700

    cap nhap game lan 7 va ban so bo bao cao

--- GameSnake/GameSnake/Game.h
+++ GameSnake/GameSnake/Game.h
@@ -18,11 +18,11 @@ private:
     bool alive = false;
     int fps = 0;
 
-    static const int FRAME_RATE = 1000 / 60;
-    static const int SCREEN_WIDTH = 640;
-    static const int SCREEN_HEIGHT = 640;
-    static const int GRID_WIDTH = 32;
-    static const int GRID_HEIGHT = 32;
+    static const int FRAME_RATE = 1000 / 20;
+    static const int SCREEN_WIDTH = 450;
+    static const int SCREEN_HEIGHT = 450;
+    static const int GRID_WIDTH = 40;
+    static const int GRID_HEIGHT = 40;
 
     SDL_Window* window = nullptr;
     SDL_Renderer* renderer = nullptr;
@@ -41,7 +41,7 @@ private:
 
     Block grid[GRID_WIDTH][GRID_HEIGHT];
 
-    float speed = 0.5f;
+    float speed = 0.6f;
     int growing = 0;
     int score = 0;
     int size = 1;
